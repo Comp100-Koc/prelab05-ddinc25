8a6d7999492ba628efa71bc4e5ad46d092487011
ddinc25 <ddinc25@ku.edu.tr>
2026-04-02 17:01:03 +0300

    done

--- q1.py
+++ q1.py
@@ -2,4 +2,15 @@ def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+    longest=''
+    for start in range(len(s)):
+        for end in range(start, len(s)):
+            possible_pal= s[start:end+1]
+            if len(possible_pal)>1:
+                 if possible_pal==possible_pal[::-1]:
+         
+                     if len(possible_pal) > len(longest):
+                         longest= possible_pal
+            
+    return longest              
+ 
